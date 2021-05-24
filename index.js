818ac0961edc589d51fa406df3ccb0989c5d1ad4
Jelena <jelena.durovic@powerhrg.com>
2021-05-24 10:52:55 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "Jelena"
+
+function upperCaseCustomerName(){
+    customerName = "BOB"
+return customerName
+}
+
+function setBestCustomer(){
+bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer(){
+ leastFavoriteCustomer = "Rob"
+
+}
